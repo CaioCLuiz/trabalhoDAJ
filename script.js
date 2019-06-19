c8cf01cc0629b5c64cafa01afe97efab7b89bda4
Thalita <thalitaribeirooliveira@gmail.com>
2019-06-18 22:11:52 -0400

    Atualização do arquivo script com a troca das cores.

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition
-var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList
-
+var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
+var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList;
 var recognition = new SpeechRecognition();
 var speechRecognitionList = new SpeechGrammarList();
 
@@ -10,58 +9,125 @@ recognition.lang = 'pt-BR';
 recognition.interimResults = false;
 recognition.maxAlternatives = 1;
 
-var g_Controller = false;
-var saida = document.querySelector('.output');
+let g_Controller = false;
+let saida = document.querySelector('.output');
 
-document.body.addEventListener('click',() =>{ recognition.start();} );
-recognition.addEventListener('speechend', () =>{ recognition.stop();} );
-recognition.addEventListener('error', (event) =>{ saida.textContent = 'Erro no reconhecimento do texto: '+ event.error;});
+document.body.addEventListener('click', () => {
+    recognition.start();
+});
+recognition.addEventListener('speechend', () => {
+    recognition.stop();
+});
+recognition.addEventListener('error', (event) => {
+    saida.textContent = 'Erro no reconhecimento do texto: ' + event.error;
+});
 
-recognition.onresult = function(event) 
-{
-    var last = event.results.length -1;
-    var texto = event.results[last][0].transcript;
-    saida.textContent = 'Resultado recebido: '+ texto + '.';
+recognition.onresult = function (event) {
+    let last = event.results.length - 1;
+    let texto = event.results[last][0].transcript;
+    saida.textContent = 'Resultado recebido: ' + texto + '.';
     disparaEvento(texto);
+};
+
+// Auxiliar
+// Promessa para trocar o valor de g_Controller
+let verificaVariavelGlobal = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        if (g_Controller)
+            resolve('A promessa foi resolvida');
+        else
+            reject('A promessa foi rejeitada.');
+    }, 10000);
+});
+
+// Array de cores usadas
+let cores = ['#999', '#03f', '#ff6', '#F00', '#60c', '#ff0'];
+
+// Seleciona as classes das td da table #daj
+let elementos = [
+    document.getElementsByClassName("es")[0],
+    document.getElementsByClassName("pr")[0],
+    document.getElementsByClassName("cl")[0],
+    document.getElementsByClassName("pro")[0],
+    document.getElementsByClassName("ca")[0],
+    document.getElementsByClassName("bo")[0]
+];
+
+// Função de troca de cores no sentido anti-horário
+function trocaCoresAntiHorario(vez) {
+    let i = 0;
+    for (elemento of elementos) {
+        elemento.style.backgroundColor = cores[vez + i];
+
+        if (vez + i < 5) {
+            console.log("valor de vez+i=" + (vez + i));
+            i++;
+        } else {
+            console.log("chegou no 5");
+            console.log("valor de vez+i=" + (vez + i));
+            i = 0 - vez;
+        }
+    }
+    if (vez < 5) {
+        vez++;
+    } else {
+        vez = 0;
+    }
 }
 
-let p = new Promise ( (resolve, reject) => {
-  setTimeout(() => {
-    if(g_Controller)
-        resolve('A promessa foi resolvida');
-    else  
-        reject ('A promessa foi rejeitada.');
-  }, 10000);
-})
+// Função de troca de cores no sentido horário
+function trocaCoresHorario(vez) {
+    let i = 0;
+    for (elemento of elementos) {
+        elemento.style.backgroundColor = cores[5 - (vez + i)];
+        console.log("valor de vez = " + vez);
+        console.log("valor de i = " + i);
+        console.log("valor de 5-(vez+i) =" + (5 - (vez + i)));
+        if (vez + i > 0) {
+            i--;
+        } else {
+            console.log("chegou no 0");
+            i = 5 - vez;
+        }
+    }
+    if (vez < 5) {
+        vez++;
+    } else {
+        vez = 0;
+    }
+}
 
 //1-IMPLEMENTAR..... FUNCAO QUE IRA DISPARAR A ACAO CORRESPONDENTE A PALAVRA
-function disparaEvento(palavra)
-{
-  if (palavra == "escopo"){
-    console.log('Recebeu escopo');
-    console.log(g_Controller);
-    g_Controller = !g_Controller;
-    console.log(g_Controller);
-  }
-  else if (palavra == "promessa"){
-    console.log('Recebeu promessa');
+function disparaEvento(palavra) {
+    if (palavra === "escopo") {
+        console.log('Recebeu escopo');
+        console.log("Variável g_Controller é " + g_Controller);
+        g_Controller = !g_Controller;
+        console.log("Variável g_Controller é " + g_Controller);
+    }
+    else if (palavra === "promessa") {
+        console.log('Recebeu promessa');
 
-    p.then( (mensagem) =>
-             {alert(mensagem);} )
-        .catch( (mensagem) =>
-            {alert(mensagem);});
-  }
-  else if (palavra == "borbulhamento")
-    console.log('Recebeu borbulhamento');
-  else
-    console.log('Recebeu outra palavra');
+        verificaVariavelGlobal
+            .then((mensagem) => {
+                alert(mensagem);
+                trocaCoresHorario(1);
+            })
+            .catch((mensagem) => {
+                alert(mensagem);
+                trocaCoresAntiHorario(1);
+            });
+    }
+    else if (palavra === "borbulhamento")
+        console.log('Recebeu borbulhamento');
+    else
+        console.log('Recebeu outra palavra');
 
 }
 
-recognition.onspeechend = function() {
+recognition.onspeechend = function () {
     recognition.stop();
-  }
-  
+};
 
 //2-IMPLEMENTAR FUNCAO DA PROMESSA
 
